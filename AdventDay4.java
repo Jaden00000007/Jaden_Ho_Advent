4e4bbf23c6ae75e4065f2196ee927ff6a0dcdb1e
Jaden00000007 <146095161+Jaden00000007@users.noreply.github.com>
2024-12-13 14:06:29 -0500

    Update AdventDay4.java

--- AdventDay4.java
+++ AdventDay4.java
@@ -11,18 +11,43 @@ public class AdventDay4 {
             String row = fileData.get(i);
             for(int j = 0; j < row.length(); j++){
                 if(row.charAt(j) == 'X'){
-                    if(row.length() - j > 4){
+                    if(row.length() - j >= 4){
                         if(row.charAt(j+1) == 'M' && row.charAt(j+2) == 'A' && row.charAt(j+3) == 'S'){
                             count++;
                         }
                     }
-                    if(j > 4){
+                    if(j >= 4){
                         if(row.charAt(j-1) == 'M' && row.charAt(j-2) == 'A' && row.charAt(j-3) == 'S'){
                             count++;
                         }
                     }
-                    if(fileData.size() - i > ){
-                        if(row.charAt(i+1) == 'M' && row.charAt(i+2) == 'A' && row.charAt(i+3) == 'S'){
+                    if(fileData.size() - i >= 4){
+                        if(fileData.get(i+1).charAt(j) == 'M' && fileData.get(i+2).charAt(j)=='A' && fileData.get(i+3).charAt(j)=='S'){
+                            count++;
+                        }
+                    }
+                    if(i >= 4){
+                        if(fileData.get(i-1).charAt(j) == 'M' && fileData.get(i-2).charAt(j)=='A' && fileData.get(i-3).charAt(j)=='S'){
+                            count++;
+                        }
+                    }
+                    if(row.length() - j >= 4 && fileData.size() - i >= 4){
+                        if(fileData.get(i+1).charAt(j+1) == 'M' && fileData.get(i+2).charAt(j+2)=='A' && fileData.get(i+3).charAt(j+3)=='S'){
+                            count++;
+                        }
+                    }
+                    if(j >= 4 && i >= 4){
+                        if(fileData.get(i-1).charAt(j-1) == 'M' && fileData.get(i-2).charAt(j-2)=='A' && fileData.get(i-3).charAt(j-3)=='S'){
+                            count++;
+                        }
+                    }
+                    if(row.length() - j >= 4 && i >= 4){
+                        if(fileData.get(i-1).charAt(j+1) == 'M' && fileData.get(i-2).charAt(j+2)=='A' && fileData.get(i-3).charAt(j+3)=='S'){
+                            count++;
+                        }
+                    }
+                    if(j >= 4 && fileData.size() - i >= 4){
+                        if(fileData.get(i+1).charAt(j-1) == 'M' && fileData.get(i+2).charAt(j-2)=='A' && fileData.get(i+3).charAt(j-3)=='S'){
                             count++;
                         }
                     }
@@ -49,3 +74,4 @@ public class AdventDay4 {
         }
     }
 }
+
